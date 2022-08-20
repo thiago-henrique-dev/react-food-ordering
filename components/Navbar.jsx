b674cc1348ee356fa8e4c995f124c38caaf3dc99
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-08-19 21:48:27 -0300

    next js basic and navbar design

---
+++ components/Navbar.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image
+            src="/img/telephone.png"
+            alt=""
+            width={32}
+            height={32}
+          />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW</div>
+          <div className={styles.text}>012 345 678</div>
+        </div>
+      </div>
+      <div className={styles.item}></div>
+    </div>
+  );
+}
